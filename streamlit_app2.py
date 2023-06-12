725c99cff0562385bb799f7aff519bc5bd51e804
Rodrigo Guarneros <rodrigoguarneros330@hotmail.com>
2023-06-12 15:12:10 -0600

    Update streamlit_app2.py
    method Client() instead of client()

--- streamlit_app2.py
+++ streamlit_app2.py
@@ -16,7 +16,7 @@ import json
 
 key_dict = json.loads(st.secrets["textkey"])
 creds = service_account.Credentials.from_service_account_info(key_dict)
-db = firestore.client(credentials=creds, project="names-project-demo")
+db = firestore.Client(credentials=creds, project="names-project-demo")
 
 dbNames = db.collection("names")
 st.header("Nuevo registro")
